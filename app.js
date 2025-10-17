721c0136df544ce2542dfeb01157b7a162052966
Fernandobika <alves.rodrigues.fernando@escola.pr.gov.br>
2025-10-17 08:17:53 -0300

    .

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ function criaCartao(categoria, perguta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao'
-}
 
-cartao.innerHTML = 
+
+cartao.innerHTML = `
     <div class="cartao_conteudo">
-               <h3>Progamação</h3>
+               <h3>${categoria}</h3>
                <div class="cartao_conteudo_pergunta">
-                    <p>O que é JavaScript?</p>
+                    <p>${pergunta}</p>
                </div>
              <div class="cartao_conteudo_pergunta">
-                <p>O javaScript é uma linguagem de Progamação</p>
+                <p>${resposta}</p>
             </div>
-             </div>
+            </div>
+            `
+            container.appendChild(cartao)
+}
